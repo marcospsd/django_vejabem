d53c9d0b6e932077df41ff22928f7c54677e8f9a
Marcos Paulo <marcosp_sd@hotmail.com>
2021-08-16 23:16:32 -0300

    buscar

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="index"),
     path("historico/", views.HistoricoListView.as_view(), name='historico'),
     path("posts/<post_id>", views.post),
-
+    path("search/", views.searchpost, name='searchpost'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from django.shortcuts import render, HttpResponse
 from django.urls import reverse_lazy
+import django_filters
 
 # Create your views here.
 from core.models import Post
@@ -20,3 +21,12 @@ class HistoricoListView(ListView):
 def post(request, post_id):
     post = Post.objects.get(slug=post_id)
     return render(request, 'post.html', {'post': post})
+
+
+def searchpost(request):
+    if request.method == "POST":
+        search = request.POST['search']
+        print(search)
+        post = Post.objects.filter(title__icontains=search)
+        print(post)
+        return render(request, 'search.html', {'post': post})
